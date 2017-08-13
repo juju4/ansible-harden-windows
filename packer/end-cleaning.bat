fc6b6c2470ff74aa0220f0e487cad0718ac74a84
juju4 <juju4@users.noreply.github.com>
2017-08-13 18:03:29 -0400

    packer: end-cleaning.bat, sysprep

---
+++ packer/end-cleaning.bat
@@ -0,0 +1,7 @@
+@echo off
+rem http://www.hurryupandwait.io/blog/creating-windows-base-images-for-virtualbox-and-hyper-v-using-packer-boxstarter-and-vagrant
+rem https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/clean-up-the-winsxs-folder
+
+@echo on
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+Defrag.exe c: /H
